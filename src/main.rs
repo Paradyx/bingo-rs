f1dd3d9a26b765b24b94971673b482ffe31ce645
Thibaud Kehler <thibaud.kehler@syss.de>
2023-01-02 16:42:52 +0100

    Some Improvements

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,43 @@
 use build_html::{Container, ContainerType, Html, HtmlContainer, HtmlPage};
 use rand::seq::SliceRandom;
 use rand::thread_rng;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
 use std::iter::repeat;
 use std::{fs::File, path::PathBuf};
 
-use clap::{arg, command, value_parser};
+use clap::{arg, command, value_parser, ArgGroup};
 
 fn main() -> std::io::Result<()> {
     let matches = command!()
         .arg(arg!(-f --file <FILE> "file containing names")
                   .value_parser(value_parser!(PathBuf))
-                  .group("input")
               )
         .arg(arg!(-l --ldap <URL> "url to the ldap OU, example ldap://ds.example.com:389/dc=example,dc=com")
-             .group("input")
              )
+        .group(ArgGroup::new("input")
+            .args(["file", "ldap"])
+            .multiple(false)
+            .required(true))
         .arg(arg!(-x --width <WIDTH> "width of the grid")
              .value_parser(value_parser!(usize))
+             .required(true)
              )
         .arg(arg!(-y --height <HEIGHT> "height of the grid")
              .value_parser(value_parser!(usize))
+             .required(true)
              )
-        .arg(arg!(-o --output <OUTPUT> "file for the HTML output"))
+        .arg(arg!(-o --output <OUTPUT> "file for the HTML output (default: STDOUT)")
+             .value_parser(value_parser!(PathBuf))
+             )
         .arg(arg!(-c --center <TEXT> "replace center cell with custom text"))
+        .arg(arg!(-d --default <TEXT> "default name, if list runs out of names"))
         .get_matches();
 
     let mut names: Vec<String> = if let Some(path) = matches.get_one::<PathBuf>("file") {
-        let file = BufReader::new(File::open(&path)?);
+        let file = BufReader::new(File::open(path)?);
         file.lines().collect::<std::io::Result<Vec<String>>>()?
     } else if let Some(ldap) = matches.get_one::<String>("ldap") {
-        todo!("load from ldap");
+        todo!("load from ldap")
     } else {
         unreachable!()
     };
@@ -45,8 +52,9 @@ fn main() -> std::io::Result<()> {
     let mut rng = thread_rng();
     names.as_mut_slice().shuffle(&mut rng);
 
-    let default_name = String::from("Gustav Geier");
-    let default_iter = repeat(&default_name);
+    let fallback_name = String::from("Joker");
+    let default_name = matches.get_one::<String>("default").unwrap_or(&fallback_name);
+    let default_iter = repeat(default_name);
 
     let mut bingo = Container::new(ContainerType::Div).with_attributes([("class", "bingo-grid")]);
 
@@ -98,7 +106,11 @@ fn main() -> std::io::Result<()> {
             width, height
         ))
         .with_container(bingo);
-
-    println!("{}", html.to_html_string());
-    Ok(())
+    
+    let mut out: Box<dyn Write> = if let Some(outpath) = matches.get_one::<PathBuf>("output") {
+        Box::new(File::create(outpath)?)
+    } else {
+        Box::new(std::io::stdout())
+    };
+    out.write_all(&html.to_html_string().as_bytes())
 }
